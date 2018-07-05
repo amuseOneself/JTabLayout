496c19ed7e4223bee67f105e9183a2fe4fe6a110
DESKTOP-516JS1P\Jianbo <563773219@qq.com>
2018-07-05 17:59:39 +0800

    07.04

--- app/src/main/java/com/liang/jtablayout/MainActivity.java
+++ app/src/main/java/com/liang/jtablayout/MainActivity.java
@@ -1,16 +1,19 @@
 package com.liang.jtablayout;
 
+import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 
 import com.liang.jtab.JTabLayout;
 import com.liang.jtab.SlidingTabStrip;
+import com.liang.jtab.TabView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +26,20 @@ public class MainActivity extends AppCompatActivity {
     private ViewPagerAdapter adapter;
     private List<View> views = new ArrayList<>();
 
+    private EditText editText1;
+    private EditText editText2;
+
+    private LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scrollView = findViewById(R.id.JTabLayout);
         viewPager = findViewById(R.id.ViewPager);
+        editText1 = findViewById(R.id.button3);
+        editText2 = findViewById(R.id.button4);
+        linearLayout = findViewById(R.id.LinearLayout);
         adapter = new ViewPagerAdapter(views);
         viewPager.setAdapter(adapter);
         layout = scrollView.getTabStrip();
@@ -42,8 +53,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 //                scrollView.addTab(getLayoutInflater().inflate(R.layout.flash_one, null));
-                views.add(getLayoutInflater().inflate(R.layout.flash_one, null));
-                adapter.notifyDataSetChanged();
+//                views.add(getLayoutInflater().inflate(R.layout.flash_one, null));
+//                adapter.notifyDataSetChanged();
+
+                String str = editText1.getText().toString();
+                if (str.isEmpty()) {
+                    scrollView.addTab(new TabView(MainActivity.this).setTitle("123456").setTitleColor(Color.GRAY, Color.BLUE));
+                } else {
+                    scrollView.addTab(new TabView(MainActivity.this).setTitle("添加"), Integer.parseInt(str));
+                }
+
+
             }
         });
 
@@ -51,11 +71,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 //                scrollView.addTab(getLayoutInflater().inflate(R.layout.flash_one, null));
-                if (views.size() > 0) {
-                    views.remove(views.size() - 1);
-                    adapter.notifyDataSetChanged();
-                }
+//                if (views.size() > 0) {
+//                    views.remove(views.size() - 1);
+//                    adapter.notifyDataSetChanged();
+//                }
 
+                String str = editText2.getText().toString();
+                if (str.isEmpty()) {
+                    scrollView.removeAllTabs();
+                } else {
+                    scrollView.removeTabAt(Integer.parseInt(str));
+                }
             }
         });
     }
--- jtablayout_lib/src/main/java/com/liang/jtab/JTabLayout.java
+++ jtablayout_lib/src/main/java/com/liang/jtab/JTabLayout.java
@@ -1,12 +1,19 @@
 package com.liang.jtab;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
 import android.database.DataSetObserver;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.LayoutInflater;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
@@ -18,16 +25,39 @@ import java.util.List;
 import static android.content.ContentValues.TAG;
 
 
-public class JTabLayout extends HorizontalScrollView {
+public class JTabLayout extends HorizontalScrollView implements ViewPager.OnPageChangeListener,
+        ViewPager.OnAdapterChangeListener {
 
     public static final int MODE_SCROLLABLE = 0;
     public static final int MODE_FIXED = 1;
+
     private SlidingTabStrip tabStrip;
-    private int mode = MODE_FIXED;
+    private int mode;
 
     private ViewPager viewPager;
     private PagerAdapter mPagerAdapter;
-    private List<View> tabViews;
+    private PagerAdapterObserver adapterObserver;
+
+    private List<Tab> tabViews;
+    private Tab selectedTab;
+
+    private int tabPaddingStart = 10;
+    private int tabPaddingTop = 0;
+    private int tabPaddingEnd = 10;
+    private int tabPaddingBottom = 0;
+
+    private ColorStateList tabTextColors;
+
+    private ValueAnimator scrollAnimator;
+
+    private ArrayList<OnTabSelectedListener> tabSelectedListeners = new ArrayList<>();
+
+
+    private int dividerWidth;
+    private int dividerHeight;
+    private int dividerColor;
+    private Paint dividerPaint;
+
 
     public JTabLayout(Context context) {
         this(context, null);
@@ -41,8 +71,102 @@ public class JTabLayout extends HorizontalScrollView {
         super(context, attrs, defStyleAttr);
         tabViews = new ArrayList<>();
         tabStrip = new SlidingTabStrip(context);
-        addView(tabStrip, new ViewGroup.LayoutParams(
+        addView(tabStrip, 0, new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.JTabLayout,
+                defStyleAttr, 0);
+
+        if (typedArray.hasValue(R.styleable.JTabLayout_tabTextColor)) {
+            tabTextColors = typedArray.getColorStateList(R.styleable.JTabLayout_tabTextColor);
+        }
+
+        mode = typedArray.getInt(R.styleable.JTabLayout_tabMode, MODE_FIXED);
+        dividerWidth = typedArray.getDimensionPixelSize(R.styleable.JTabLayout_dividerWidth, 0);
+        dividerHeight = typedArray.getDimensionPixelSize(R.styleable.JTabLayout_dividerHeight, 50);
+        dividerColor = typedArray.getColor(R.styleable.JTabLayout_dividerColor, Color.BLACK);
+
+        typedArray.recycle();
+    }
+
+    public void addOnTabSelectedListener(@NonNull OnTabSelectedListener listener) {
+        if (!tabSelectedListeners.contains(listener)) {
+            tabSelectedListeners.add(listener);
+        }
+    }
+
+    public void removeOnTabSelectedListener(@NonNull OnTabSelectedListener listener) {
+        tabSelectedListeners.remove(listener);
+    }
+
+    public void dispatchTabSelected(Tab tab) {
+        if (tab == null) {
+            return;
+        }
+        for (int i = tabSelectedListeners.size() - 1; i >= 0; i--) {
+            tabSelectedListeners.get(i).onTabSelected(tab.getPosition());
+        }
+    }
+
+    public void dispatchTabUnselected(Tab tab) {
+        if (tab == null) {
+            return;
+        }
+        for (int i = tabSelectedListeners.size() - 1; i >= 0; i--) {
+            tabSelectedListeners.get(i).onTabUnselected(tab.getPosition());
+        }
+    }
+
+    private void dispatchTabReselected(Tab tab) {
+        if (tab == null) {
+            return;
+        }
+        for (int i = tabSelectedListeners.size() - 1; i >= 0; i--) {
+            tabSelectedListeners.get(i).onTabReselected(tab.getPosition());
+        }
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
+        updateTabViews(true);
+    }
+
+    public void setTabPadding(int mTabPaddingStart, int mTabPaddingTop, int mTabPaddingEnd, int mTabPaddingBottom) {
+        this.tabPaddingStart = mTabPaddingStart;
+        this.tabPaddingTop = mTabPaddingTop;
+        this.tabPaddingEnd = mTabPaddingEnd;
+        this.tabPaddingBottom = mTabPaddingBottom;
+        updateTabViews(true);
+    }
+
+    public void updateTabViews(boolean requestLayout) {
+        switch (mode) {
+            case MODE_FIXED:
+                tabStrip.setGravity(Gravity.CENTER);
+                break;
+            case MODE_SCROLLABLE:
+                tabStrip.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+                break;
+        }
+        for (int i = 0; i < tabStrip.getChildCount(); i++) {
+            View child = tabStrip.getChildAt(i);
+            child.setPadding(tabPaddingStart, tabPaddingTop, tabPaddingEnd, tabPaddingBottom);
+            updateTabViewLayoutParams((LinearLayout.LayoutParams) child.getLayoutParams(), i == 0 ? 0 : dividerWidth);
+            if (requestLayout) {
+                child.requestLayout();
+            }
+        }
+    }
+
+    private void updateTabViewLayoutParams(LinearLayout.LayoutParams lp, int dividerWidth) {
+        lp.leftMargin = dividerWidth;
+        if (mode == MODE_FIXED) {
+            lp.width = 0;
+            lp.weight = 1.0f;
+        } else {
+            lp.width = LinearLayout.LayoutParams.WRAP_CONTENT;
+            lp.weight = 0;
+        }
     }
 
     @Override
@@ -68,57 +192,195 @@ public class JTabLayout extends HorizontalScrollView {
         }
     }
 
-    public void setViewPager(ViewPager viewPager) {
-        this.viewPager = viewPager;
-        mPagerAdapter = viewPager.getAdapter();
-        mPagerAdapter.registerDataSetObserver(new PagerAdapterObserver());
+    public Tab newTab() {
+        return new TabView(getContext());
     }
 
+    public void setViewPager(ViewPager viewPager) {
+        setViewPager(viewPager, true);
+    }
+
+    public void setViewPager(ViewPager viewPager, boolean autoRefresh) {
+
+        if (this.viewPager != null) {
+            this.viewPager.removeOnPageChangeListener(this);
+            this.viewPager.removeOnAdapterChangeListener(this);
+        }
+
+        if (viewPager != null) {
+            this.viewPager = viewPager;
+            viewPager.addOnPageChangeListener(this);
+            viewPager.addOnAdapterChangeListener(this);
+        }
+
+        final PagerAdapter adapter = viewPager.getAdapter();
+        if (adapter != null) {
+            if (mPagerAdapter != null && adapterObserver != null) {
+                mPagerAdapter.unregisterDataSetObserver(adapterObserver);
+            }
+
+            mPagerAdapter = viewPager.getAdapter();
+
+            if (autoRefresh) {
+                if (adapterObserver == null) {
+                    adapterObserver = new PagerAdapterObserver();
+                }
+                adapter.registerDataSetObserver(adapterObserver);
+                populateFromPagerAdapter();
+            }
+        }
+    }
+
+    private void populateFromPagerAdapter() {
+        removeAllTabs();
+        if (mPagerAdapter != null) {
+            final int adapterCount = mPagerAdapter.getCount();
+            for (int i = 0; i < adapterCount; i++) {
+                addTab(newTab().setTitle(mPagerAdapter.getPageTitle(i)), false);
+            }
+
+            if (viewPager != null && adapterCount > 0) {
+                final int curItem = viewPager.getCurrentItem();
+                if (curItem != getSelectedTabPosition() && curItem < getTabCount()) {
+                    selectTab(getTabAt(curItem));
+                }
+            }
+        }
+    }
+
+    private void selectTab(Tab tab) {
+        if (selectedTab != null) {
+            selectedTab.setSelected(false);
+            dispatchTabUnselected(selectedTab);
+        }
+
+        tab.setSelected(true);
+        dispatchTabSelected(tab);
+
+        selectedTab = tab;
+    }
 
     public SlidingTabStrip getTabStrip() {
         return tabStrip;
     }
 
-
-    public void addTab(View view) {
-        addTab(view, tabViews.isEmpty());
+    public int getTabCount() {
+        return tabViews.size();
     }
 
-    public void addTab(View view, int position) {
-        addTab(view, position, tabViews.isEmpty());
+    @Nullable
+    public Tab getTabAt(int index) {
+        return (index < 0 || index >= getTabCount()) ? null : tabViews.get(index);
     }
 
-    public void addTab(View view, boolean setSelected) {
-        addTab(view, tabViews.size(), setSelected);
+    public int getSelectedTabPosition() {
+        return selectedTab != null ? selectedTab.getPosition() : -1;
     }
 
-    public void addTab(View view, int position, boolean setSelected) {
-        tabStrip.addView(view, new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f));
+    public void addTab(Tab tab) {
+        addTab(tab, tabViews.isEmpty());
     }
 
-    private void configureTab(View view, int position) {
-//        view.setPosition(position);
-        tabViews.add(position, view);
+    public void addTab(Tab tab, int position) {
+        addTab(tab, position, tabViews.isEmpty());
+    }
 
-        final int count = tabViews.size();
-        for (int i = position + 1; i < count; i++) {
-//            tabViews.get(i).setPosition(i);
+    public void addTab(Tab tab, boolean setSelected) {
+        addTab(tab, tabViews.size(), setSelected);
+    }
+
+    public void addTab(Tab tab, int position, boolean setSelected) {
+        configureTab(tab, position);
+        addTabView(tab);
+
+        if (setSelected) {
+            selectTab(tab);
         }
     }
 
-    private void addTabView(View view) {
+    private void configureTab(Tab tab, int position) {
+        tab.setPosition(position);
+        tabViews.add(position, tab);
+
+        final int count = tabViews.size();
+        for (int i = position + 1; i < count; i++) {
+            tabViews.get(i).setPosition(i);
+        }
+    }
+
+    private void addTabView(Tab tab) {
+        Log.e("addTabView", "tab: ..." + tab.getPosition());
+
+        tab.setPadding(tabPaddingStart, tabPaddingTop, tabPaddingEnd, tabPaddingBottom);
+
         LinearLayout.LayoutParams params = mode == MODE_SCROLLABLE ? new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT) :
                 new LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 1.0f);
-//        tabStrip.addView(view, view.getPosition(), params);
-        tabStrip.addView(view, params);
+        tabStrip.addView(tab, tab.getPosition(), params);
+
+        if (tab.getTitleColor() != null && tabTextColors != null) {
+            tab.setTitleColor(tabTextColors);
+        }
+
+        tab.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getSelectedTabPosition() == ((Tab) v).getPosition()) {
+                    dispatchTabReselected((Tab) v);
+                } else {
+                    selectTab((Tab) v);
+                }
+            }
+        });
+    }
+
+    public void removeTab(Tab tab) {
+        removeTabAt(tab.getPosition());
+    }
+
+    public void removeTabAt(int position) {
+        final int selectedTabPosition = selectedTab != null ? selectedTab.getPosition() : 0;
+        removeTabViewAt(position);
+        tabViews.remove(position);
+        final int newTabCount = tabViews.size();
+        for (int i = position; i < newTabCount; i++) {
+            tabViews.get(i).setPosition(i);
+        }
+
+        if (selectedTabPosition == position) {
+            selectTab(tabViews.isEmpty() ? null : tabViews.get(Math.max(0, position - 1)));
+        }
+    }
+
+    private void removeTabViewAt(int position) {
+        tabStrip.removeViewAt(position);
+        requestLayout();
     }
 
     public void removeAllTabs() {
-        // Remove all the views
         tabStrip.removeAllViews();
         tabViews.clear();
-//        selectedTab = null;
+        selectedTab = null;
         requestLayout();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onAdapterChanged(@NonNull ViewPager viewPager, @Nullable PagerAdapter oldAdapter, @Nullable PagerAdapter newAdapter) {
+
     }
 
     private class PagerAdapterObserver extends DataSetObserver {
@@ -133,25 +395,6 @@ public class JTabLayout extends HorizontalScrollView {
         @Override
         public void onInvalidated() {
             populateFromPagerAdapter();
-        }
-    }
-
-    void populateFromPagerAdapter() {
-        removeAllTabs();
-        if (mPagerAdapter != null) {
-            final int adapterCount = mPagerAdapter.getCount();
-            Log.e(TAG, "populateFromPagerAdapter adapterCount: ..." + adapterCount);
-            for (int i = 0; i < adapterCount; i++) {
-                addTab(LayoutInflater.from(getContext()).inflate(R.layout.flash_one, null));
-            }
-
-            // Make sure we reflect the currently set ViewPager item
-//            if (viewPager != null && adapterCount > 0) {
-//                final int curItem = viewPager.getCurrentItem();
-//                if (curItem != getSelectedTabPosition() && curItem < getTabCount()) {
-//                    selectTab(getTabAt(curItem));
-//                }
-//            }
         }
     }
 
